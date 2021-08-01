b6f0bbef156569892866ad519d99acb33011a9f7
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2021-08-01 17:58:02 +0200

    Lession/144/optional interface properties (#2)
    * feat: initalized User Base Class and UserProps interface
    
    * feat: add set method to user class and modify UserProps interface
    
    * chore: resolve merge conflict

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,21 @@
+<<<<<<< HEAD
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+}
+=======
 interface UserProps{
   name:string; 
   age:number;
@@ -10,3 +28,4 @@ export class User{
     return this.data[propName];
   }
 }
+>>>>>>> 6c09b38a0c97a452576df5f10c1049c0b565ac7f
