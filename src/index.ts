cc37b035cc0734264ad7d9d7d90ffdb6f335fff9
Mitchell <mwhyuen@gmail.com>
2021-08-02 14:04:50 +0200

    feat: add dynamic array collection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "Mitchell", age: 22 });
-user.set({ name: "Inez"});
-console.log(user.get("name"));
-console.log(user.get("age"));
+
+user.on('change', ()=>{
+    console.log('User changed');
+})
+
+console.log(user);
