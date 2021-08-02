d5d474879eac2c717d671e42424b4e8c4597a953
Mitchell <mwhyuen@gmail.com>
2021-08-02 14:12:14 +0200

    feat: add trigger method to User class

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Mitchell", age: 22 });
 
-user.on('change', ()=>{
-    console.log('User changed');
+user.on("change", () => {
+  console.log("User changed");
+});
+user.on("delete", () => {
+  console.log("DELETE")
 })
-
 console.log(user);
+
+user.trigger('change');
